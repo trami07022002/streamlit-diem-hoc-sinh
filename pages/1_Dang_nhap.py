0de01d3c7f325990314c0590b5c4cbefcb933598
trami07022002 <ntthanhthao1612@gmail.com>
2025-11-13 13:37:02 +0700

    Update 1_Dang_nhap.py

--- pages/1_Dang_nhap.py
+++ pages/1_Dang_nhap.py
@@ -2,11 +2,9 @@ import streamlit as st
 import json
 import os
 
-st.set_page_config(page_title="Đăng nhập")
-
 st.title("🔐 Đăng nhập hệ thống")
 
-# load user database
+# Load user database
 if os.path.exists("users.json"):
     with open("users.json", "r") as f:
         users = json.load(f)
@@ -20,13 +18,11 @@ if st.button("Đăng nhập"):
     if username in users and users[username]["password"] == password:
         st.session_state["user"] = username
         st.session_state["role"] = users[username]["role"]
-
         st.success("🎉 Đăng nhập thành công!")
-        st.switch_page("pages/3_📚_Nhập_điểm.py")
+        st.switch_page("pages/3_Nhap_diem.py")
     else:
         st.error("❌ Sai tài khoản hoặc mật khẩu")
 
 st.write("Nếu bạn chưa có tài khoản:")
-if st.button("📝 Đăng ký"):
-    st.switch_page("pages/2_📝_Đăng_ký.py")
-
+if st.button("🆕 Đăng ký"):
+    st.switch_page("pages/2_Dang_ky.py")
