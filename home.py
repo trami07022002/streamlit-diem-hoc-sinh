bd3259ab35f23625202efe04e341e75ce9a86572
trami07022002 <ntthanhthao1612@gmail.com>
2025-11-13 11:06:16 +0700

    Add files via upload

---
+++ home.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.set_page_config(page_title="Hệ thống quản lý điểm học sinh", layout="wide")
+
+st.title(" 🏫 Hệ thống quản lý điểm học sinh")
+st.write("Chọn chức năng bên dưới:")
+
+if st.button("🔐 Đăng nhập"):
+    st.switch_page("pages/1_🔐_Đăng_nhập.py")
+
+if st.button("📝 Đi đến trang nhập điểm"):
+    st.switch_page("pages/3_📚_Nhập_điểm.py")
